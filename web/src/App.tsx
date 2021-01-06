fb0f8a35a944f8b7b63cbc60b1fe898f368b1ccf
Natan <nattantavares.s15@gmail.com>
2021-01-06 15:37:19 -0300

    Base structure

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,21 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { BiSearchAlt2 } from 'react-icons/bi';
+
+import Logo from './assets/googleIcon.png';
 
 import './App.css';
 
 export default function App() {
+  const [term, setTerm] = useState('');
+
   return (
     <div className="App">
-      <h1>Google</h1>
-      {/* Logo google href="www.google.com" */}
+      <img src={Logo} alt="Google" width={200} />
+      <span>Livros</span>
 
       <div className="searchBar">
-        <input type="text" value="I'm a search bar!" />
-        <button>Lupa</button>
+        <input 
+          type="text"
+          value={term}
+          onChange={event => setTerm(event.target.value)}
+        />
+
+        <button>
+          <BiSearchAlt2 />
+        </button>
       </div>
 
-      <h3>Book finder</h3>
+      <span>O índice mais diversificado do mundo para pesquisar livros na íntegra.</span>
 
-      {/* Component of contact me */}
+      <footer>
+        <p>NTS Labs</p>
+      </footer>
     </div>
   );
 }
