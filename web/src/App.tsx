705c36aafb5250a6250619c5ea1b9a266c7fc5a5
Natan <nattantavares.s15@gmail.com>
2021-01-05 23:08:22 -0300

    Modification of the layout

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
-import Header from './components/Header';
-import ListOfCards from './components/ListOfCards';
 
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
-      <Header />
-      <ListOfCards size={1} />
+      <h1>Google</h1>
+      {/* Logo google href="www.google.com" */}
+
+      <div className="searchBar">
+        <input type="text" value="I'm a search bar!" />
+        <button>Lupa</button>
+      </div>
+
+      <h3>Book finder</h3>
+
+      {/* Component of contact me */}
     </div>
   );
 }
