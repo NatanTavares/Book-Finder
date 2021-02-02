6ea87c8caeba8146cf907bc190181a8f311f2079
Natan <nattantavares.s15@gmail.com>
2021-02-01 22:36:19 -0300

    Added routes on project (dashboard, detail)

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,40 +1,23 @@
-import React, { useState } from 'react';
-import { BiSearchAlt2 } from 'react-icons/bi';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-import Logo from './assets/googleIcon.png';
+import Dashboard from './pages/Dashboard';
+import Home from './pages/Home';
+import Detail from './pages/Detail';
 
 import './App.css';
 
 export default function App() {
-  const [term, setTerm] = useState('');
 
   return (
     <div className="App">
-      <section>
-        <div className="logo-section">
-          <img src={Logo} alt="Google" width={272} />
-          <span>Livros</span>
-        </div>
-
-        <div className="searchBar">
-          <input 
-            type="text"
-            value={term}
-            onChange={event => setTerm(event.target.value)}
-          />
-
-          <button>
-            <BiSearchAlt2 size={20} color="#FFF" />
-          </button>
-        </div>
-
-        <span>O índice mais diversificado do mundo para pesquisar livros na íntegra.</span>
-      </section>
-
-      <footer>
-        <span>Developed by </span>
-        <a href="https://linktr.ee/NatanTavares" target="blank">NTS Labs</a>
-      </footer>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/dashboard" component={Dashboard} />
+          <Route path="/detail:volume" component={Detail} />
+        </Switch>
+      </Router>
     </div>
   );
 }
