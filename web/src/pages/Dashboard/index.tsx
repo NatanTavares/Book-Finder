92cbe60b18e6d7f4782fdeb1beda0e4414d5b898
Natan <nattantavares.s15@gmail.com>
2021-02-20 16:08:55 -0300

    Added some book templates

--- web/src/pages/Dashboard/index.tsx
+++ web/src/pages/Dashboard/index.tsx
@@ -35,61 +35,61 @@ export default function Dashboard() {
       <S.Grid>
         <Card 
           url="/detail:react"
-          // img="#"
+          img="http://books.google.com/books/content?id=tDjrDwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
           volume="React"
         />
 
         <Card 
           url="/detail:node.js" 
-          // img="#" 
+          img="http://books.google.com/books/content?id=byCjCwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
           volume="Node.js"
         />
 
         <Card 
           url="/detail:next.js" 
-          // img="#"
+          img="http://books.google.com/books/content?id=xAGCzSGTMNIC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
           volume="Next.js"
         />
 
         <Card 
           url="/detail:angular.js" 
-          // img="#" 
+          img="http://books.google.com/books/content?id=deX8DwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
           volume="Angular.js"
         />
 
         <Card 
           url="/detail:vue.js" 
-          // img="#" 
+          img="http://books.google.com/books/content?id=Ft-8DgAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
           volume="Vue.js"
         />
       
         <Card 
           url="/detail:react native.js" 
-          // img="#" 
+          img="http://books.google.com/books/content?id=keDdDwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
           volume="React Native"
         />
         
         <Card 
           url="/detail:typescript" 
-          // img="#" 
+          img="http://books.google.com/books/content?id=wD63DwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
           volume="Typescript"
         />
 
         <Card 
-          url="/detail:node" 
-          // img="#" 
-          volume="Node"
+          url="/detail:javascript" 
+          img="http://books.google.com/books/content?id=6n_xDwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
+          volume="JavaScript"
         />
 
         <Card 
           url="/detail:orm" 
-          // img="#" 
+          img="http://books.google.com/books/content?id=Edc0AAAAMAAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
           volume="ORM"
         />
 
         <Card 
           url="/detail:mongodb" 
-          // img="#" 
+          img="http://books.google.com/books/content?id=pIrCDwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
           volume="MongoDB"
         />
       </S.Grid>
