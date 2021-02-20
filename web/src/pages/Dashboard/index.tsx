21f26a57c8c889b98b9f6c6ecf788a06be428c05
Natan <nattantavares.s15@gmail.com>
2021-02-20 16:50:52 -0300

    Refactored to use the Header component

--- web/src/pages/Dashboard/index.tsx
+++ web/src/pages/Dashboard/index.tsx
@@ -1,36 +1,14 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react';
 
+import Header from '../../components/Header';
 import Card from '../../components/Card/index';
 
-import { BiSearchAlt2 } from 'react-icons/bi';
-import Logo from '../../assets/googleIcon.png';
 import S from './styles';
 
 export default function Dashboard() {
-  const [term, setTerm] = useState('');
-
   return (
     <>
-      <S.Header>
-        <S.ContentHeader>
-          <Link to='/'>
-            <img src={Logo} alt="Google" width={120} />
-          </Link>
-
-          <S.InputOnHeader>
-            <S.Input 
-              type="text"
-              value={term}
-              onChange={event => setTerm(event.target.value)}
-            />
-
-            <S.Button onClick={() => console.log('> [Fetch api]: test')}>
-              <BiSearchAlt2 size={20} color="#44F" />
-            </S.Button>
-          </S.InputOnHeader>
-        </S.ContentHeader>
-      </S.Header>
+      <Header />
 
       <S.Grid>
         <Card 
