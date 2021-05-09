4535bdf3a8cebed2d6aeb2a9b172848cb1dd4a27
NatanTavares <nattantavares.s15@gmail.com>
2021-05-09 20:58:22 -0300

    refactor(detail): changes the detail page to use the stylized component

---
+++ web/src/styles/pages/Detail.ts
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const Title = styled.h2`
+  font-size: 1.6em;
+  text-align: center;
+`;
+
+export const Subtitle = styled.h3`
+  font-size: 1.2em;
+  text-align: center;
+`;
+
+const Styled = {
+  Title,
+  Subtitle,
+};
+
+export default Styled;
