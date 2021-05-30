a55b7509fdf5badc708ebdf4e07a9f2cd8c6dd85
NatanTavares <nattantavares.s15@gmail.com>
2021-05-30 09:29:53 -0300

    feat(home): Implements the visual part of the screen

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,34 @@
+import Head from "next/head";
+
+import { BiSearchAlt2 } from "react-icons/bi";
+
+import styles from "./home.module.scss";
+
 export default function Home() {
   return (
-    <div>
-      <h1>Olá</h1>
-    </div>
+    <>
+      <Head>
+        <title>Finder Books</title>
+      </Head>
+
+      <main className={styles.container}>
+        <div className={styles.logoContainer}>
+          <img src="/images/mainIcon.png" alt="Google" />
+          <span>Books</span>
+        </div>
+
+        <div className={styles.inputContainer}>
+          <input type="text" />
+
+          <button type="button">
+            <BiSearchAlt2 size={20} color="#FFF" />
+          </button>
+        </div>
+
+        <p>
+          The world's most diverse index for searching books in their entirety.
+        </p>
+      </main>
+    </>
   );
 }
