782d1d2177c7356b65c454ffed4f00c385b7fc5e
NatanTavares <nattantavares.s15@gmail.com>
2021-06-14 22:23:22 -0300

    feat(component): Adds the link that will display the book in detail"

--- src/components/Book/index.tsx
+++ src/components/Book/index.tsx
@@ -3,14 +3,14 @@ import Link from "next/link";
 import styles from "./styles.module.scss";
 
 interface BookProps {
+  id: string;
   img?: string;
   title: string;
-  detailsUrl?: string;
 }
 
-export function Book({ img, title, detailsUrl = "/book/Test" }: BookProps) {
+export function Book({ id, img, title }: BookProps) {
   return (
-    <Link href={detailsUrl}>
+    <Link href={`/book/${id}`}>
       <a className={styles.container}>
         <div className={styles.thumbnailContainer}>
           {!!img ? <img src={img} alt={title} /> : "No image available"}
