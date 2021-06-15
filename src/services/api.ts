5cf018c13a61359b8137cb795bf0ba9becd5d50d
NatanTavares <nattantavares.s15@gmail.com>
2021-06-14 22:07:13 -0300

    refactor(axios): Change the client's base url

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://www.googleapis.com/books/v1/",
+  baseURL: "https://www.googleapis.com/books/v1/volumes",
 });
-
-// https://www.googleapis.com/books/v1/volumes?q=
