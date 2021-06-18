b5b4af30fc9d0ee4dc74bf10649c58e5399a784c
NatanTavares <nattantavares.s15@gmail.com>
2021-06-17 21:56:58 -0300

    feat(home): implements the functionality that will search the books

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,18 @@
 import Head from "next/head";
+import { useRouter } from "next/router";
+import { useState } from "react";
 
 import { BiSearchAlt2 } from "react-icons/bi";
-
 import styles from "./home.module.scss";
 
 export default function Home() {
+  const [volume, setVolume] = useState("");
+  const router = useRouter();
+
+  async function handleVolume() {
+    router.push(`books/${volume}`);
+  }
+
   return (
     <>
       <Head>
@@ -18,9 +26,13 @@ export default function Home() {
         </div>
 
         <div className={styles.inputContainer}>
-          <input type="text" />
+          <input
+            type="text"
+            value={volume}
+            onChange={(e) => setVolume(e.target.value)}
+          />
 
-          <button type="button">
+          <button type="button" onClick={handleVolume}>
             <BiSearchAlt2 size={20} color="#FFF" />
           </button>
         </div>
