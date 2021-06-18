02d149a2a4075fd14aa85a0aa7af9ce923bf14f9
NatanTavares <nattantavares.s15@gmail.com>
2021-06-17 22:11:10 -0300

    feat(header): Adds the function that will redirect the user to the Books screen with the new search

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,29 @@
-import styles from "./styles.module.scss";
+import { useRouter } from "next/router";
+import { useState } from "react";
 
 import { BiSearchAlt2 } from "react-icons/bi";
+import styles from "./styles.module.scss";
 
 export function Header() {
+  const router = useRouter();
+  const [volume, setVolume] = useState("");
+
+  function handleNewVolumes() {
+    router.push(`/books/${volume}`);
+  }
+
   return (
     <header className={styles.container}>
       <div className={styles.content}>
         <img src="/images/mainIcon.png" alt="Google" />
 
         <div className={styles.inputContainer}>
-          <input type="text" />
-          <button type="button">
+          <input
+            type="text"
+            onChange={(e) => setVolume(e.target.value)}
+            placeholder="Find any book"
+          />
+          <button type="button" onClick={handleNewVolumes}>
             <BiSearchAlt2 size={20} color="#FFF" />
           </button>
         </div>
